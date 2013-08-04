4d3c9940788c9ee259227e57fd6fc2d1d5be9c2e
AmeliaMN <amelia.a.mcnamara@gmail.com>
2013-08-04 12:44:05 -0400

    removed some files

--- csatShiny/ui.R
+++
@@ -1,7 +0,0 @@
-shinyUI(bootstrapPage(
-  selectInput(inputId = "citystate",
-              label = "Community:",
-              choices =  levels(csat$citystate),
-              selected = levels(csat$citystate)[1]),
-  plotOutput(outputId = "main_plot", width = "500px")
-))
